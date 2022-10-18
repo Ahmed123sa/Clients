1bc14e5cc1514699afb7a174c70830e3a0167e4d
Ahmed <aaasa202@gmail.com>
2022-10-18 18:27:26 +0200

    second commit

--- plugins.js
+++ plugins.js
@@ -1,6 +1,9 @@
-let input = document.querySelector('input')
+let input = document.querySelector('input');
+let inputW=document.querySelector('.weight');
+let inputP =document.getElementById('phone');
 let insert = document.getElementById('ins');
 let containDiv = document.querySelector('.details');
+
 let RenewD='create'
 let namesArray;
 if(localStorage.data != null){
@@ -19,6 +22,9 @@ insert.onclick=(function(e){
   const data = {
     name: input.value,
     date: new Date().toLocaleString(), 
+    weight: inputW.value,
+    phone: inputP.value,
+    
   }
   namesArray.push(data);
   localStorage.setItem('data',JSON.stringify(namesArray));
@@ -38,35 +44,38 @@ function clearData(){
 function addToPage(){
   let botDiv = '';
   for(let i=0; i<namesArray.length; i++){
+    
     botDiv += `
-         <div class="bot">
-        <div class="name">${namesArray[i].name}<div class="date">${namesArray[i].date} </div></div>
-        
-       
-         <button onclick="deletData(${i})" class="btn btn-danger" id="del">Delete</button>
-         <button onclick="Renew(${i})" id="Ren" class="btn btn-primary">Renew</button>
-     </div>
+        <div class="bot">
+          <span>${i+1}</span><div class="name">${namesArray[i].name} - ${namesArray[i].weight} - ${namesArray[i].phone}
+          <div class="date">${namesArray[i].date} </div>          
+          </div>
+           <button onclick="deletData(${i})" class="btn btn-danger" id="del">Delete</button>
+           <button onclick="Renew(${i})" id="Ren" class="btn btn-primary">Renew</button>
+        </div>
     `
   }
   containDiv.innerHTML=botDiv;
+  
+  
   for(let i=0; i<namesArray.length; i++){
+    
     let theDate = new Date (namesArray[i].date);
     let newDate = new Date();
-    if(theDate.getDate() <= newDate.getDate()){
+    if(theDate.getDate() <= newDate.getDate() && theDate.getMonth() < newDate.getMonth()){
       $('.name').css('background','red')
       $('.name').css('color','white')
     }
     
-  
-  
   }
 }
 //delet
 function deletData(i){
   let del = JSON.stringify(namesArray.splice(i,1));
-  console.log(del)
-namesArray.splice(i,1);
-localStorage.data= JSON.stringify(namesArray);
+  
+  
+  namesArray.splice(i,0);
+ localStorage.data= JSON.stringify(namesArray);
 addToPage()
 }
 //////
@@ -101,7 +110,24 @@ function searchNames(value)
   } 
   containDiv.innerHTML=botDiv;
 }
+//////
+let month=document.getElementById('month');
+let newCont = document.querySelector('.new')
 
-
-
- 
+if(month.innerText < new Date().getMonth()){
+  let botDiv = '';
+  for(let i=0; i<namesArray.length; i++){
+    
+    botDiv += `
+        <div class="bot">
+          <span>${i+1}</span><div class="name">${namesArray[i].name}
+          <div class="date">${namesArray[i].date }</div>
+          <div>${namesArray[i].weight}</div>
+                    </div>
+           <button onclick="deletData(${i})" class="btn btn-danger" id="del">Delete</button>
+           <button onclick="Renew(${i})" id="Ren" class="btn btn-primary">Renew</button>
+        </div>
+    `
+  }
+  newCont.innerHTML=botDiv;
+}
